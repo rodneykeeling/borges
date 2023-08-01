913b00ca26eb8d52a82dd9d07ff0c5029d14e7d9
Rodney Keeling <rodneykeeling@gmail.com>
2023-07-31 18:20:57 -0600

    Switch in-memory datastore to more efficient data structure

--- src/repository.rs
+++ src/repository.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use crate::graphql::Book;
 use axum::async_trait;
 
@@ -6,25 +8,31 @@ pub trait BookRepository {
     fn get_by_title(&self, title: String) -> Option<&Book>;
 }
 
+// In-memory datastore used for tests/testing. Uses a HashMap instead of Vec for O(1) gets and inserts
 pub struct InMemoryBookRepository {
-    pub db: Vec<Book>,
+    pub db: HashMap<String, Book>,
 }
 
 impl InMemoryBookRepository {
     pub fn new() -> Self {
-        let db = vec![Book {
-            title: "Collected Fictions".to_string(),
-            author: "Jorge Luis Borges".to_string(),
-            image_url: None,
-            year: 1998,
-            pages: 565,
-        }];
+        let mut db = HashMap::new();
+        let title = "Collected Fictions".to_string();
+        db.insert(
+            title.clone(),
+            Book {
+                title,
+                author: "Jorge Luis Borges".to_string(),
+                image_url: None,
+                year: 1998,
+                pages: 565,
+            },
+        );
         Self { db }
     }
 }
 
 impl BookRepository for InMemoryBookRepository {
     fn get_by_title(&self, title: String) -> Option<&Book> {
-        self.db.iter().find(|&book| book.title == title)
+        self.db.get(&title)
     }
 }
