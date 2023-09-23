e487f6ce0756ad4996a06add558b6994bb4d1416
Rodney Keeling <rodneykeeling@gmail.com>
2023-09-22 19:38:39 -0600

    Add function to handle test requests

--- tests/approval.rs
+++ tests/approval.rs
@@ -22,7 +22,7 @@ const BOOK_QUERY: &str = "
     }
 ";
 
-const NOTE_MUTATION: &str = "
+const ADD_NOTE_MUTATION: &str = "
     mutation {
       addNote(input: {bookId: 1, note: \"new note!\", page: 3}) {
         id
@@ -41,58 +41,50 @@ struct Request {
     variables: Option<String>,
 }
 
-#[sqlx::test]
-async fn test_graphql(pool: Pool<Postgres>) -> sqlx::Result<()> {
+async fn _run_request(request_body: Request, pool: Pool<Postgres>) -> Value {
     let app = generate_app(pool).await.unwrap();
 
+    let resp = app
+        .oneshot(
+            axum::http::Request::builder()
+                .method(axum::http::Method::POST)
+                .uri("/")
+                .body(axum::body::Body::from(
+                    serde_json::to_string(&request_body).unwrap(),
+                ))
+                .unwrap(),
+        )
+        .await
+        .unwrap();
+    let body = hyper::body::to_bytes(resp.into_body()).await.unwrap();
+    let result: Value = serde_json::from_slice(&body).unwrap();
+
+    result
+}
+
+#[sqlx::test]
+async fn test_book_query(pool: Pool<Postgres>) -> sqlx::Result<()> {
     let body = Request {
         query: BOOK_QUERY.to_string(),
         operation_name: None,
         variables: None,
     };
 
-    let resp = app
-        .oneshot(
-            axum::http::Request::builder()
-                .method(axum::http::Method::POST)
-                .uri("/")
-                .body(axum::body::Body::from(
-                    serde_json::to_string(&body).unwrap(),
-                ))
-                .unwrap(),
-        )
-        .await
-        .unwrap();
-    let body = hyper::body::to_bytes(resp.into_body()).await.unwrap();
-    let result: Value = serde_json::from_slice(&body).unwrap();
+    let result = _run_request(body, pool).await;
     insta::assert_json_snapshot!(result);
 
     Ok(())
 }
 
 #[sqlx::test]
-async fn test_mutation(pool: Pool<Postgres>) -> sqlx::Result<()> {
-    let app = generate_app(pool).await.unwrap();
-
+async fn test_add_note_mutation(pool: Pool<Postgres>) -> sqlx::Result<()> {
     let body = Request {
-        query: NOTE_MUTATION.to_string(),
+        query: ADD_NOTE_MUTATION.to_string(),
         operation_name: None,
         variables: None,
     };
-    let resp = app
-        .oneshot(
-            axum::http::Request::builder()
-                .method(axum::http::Method::POST)
-                .uri("/")
-                .body(axum::body::Body::from(
-                    serde_json::to_string(&body).unwrap(),
-                ))
-                .unwrap(),
-        )
-        .await
-        .unwrap();
-    let body = hyper::body::to_bytes(resp.into_body()).await.unwrap();
-    let result: Value = serde_json::from_slice(&body).unwrap();
+
+    let result = _run_request(body, pool).await;
     insta::assert_json_snapshot!(result);
 
     Ok(())
